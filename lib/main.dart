492a72de5260845712c9488cf6d1fcf954082963
matthewwarner <mattwarner76@gmail.com>
2019-03-30 12:54:44 -0400

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:four_some/services/authentication.dart';
+import 'package:four_some/pages/root_page.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+        title: '4some',
+        debugShowCheckedModeBanner: false,
+        theme: new ThemeData(
+          primarySwatch: Colors.lightGreen,
+        ),
+        home: new RootPage(auth: new Auth()));
+  }
+}
